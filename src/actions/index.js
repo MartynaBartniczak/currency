97c2e7b7f003bd960cffef67e1122130869f5bc6
MartynaBartniczak <martyna@24nettbutikk.no>
2019-04-09 12:49:26 +0200

    actions changed

--- src/actions/index.js
+++ src/actions/index.js
@@ -2,14 +2,27 @@ import axios from "axios";
 
 const ROOT_URL = `http://api.nbp.pl/api/exchangerates/tables/a`;
 export const FETCH_CURRENCY = "FETCH_CURRENCY";
+export const FETCH_CURRENCY_FAILURE = "FETCH_CURRENCY_FAILURE";
 
-export async function fetchCurrency(date) {
+export function fetchCurrency(date) {
   const url = `${ROOT_URL}/${date}/`;
-  const request = await axios.get(url);
-  console.log(request, "request");
 
-  return {
-    type: FETCH_CURRENCY,
-    payload: request
+  return function(dispatch) {
+    axios
+      .get(url)
+      .then(response => {
+        console.log(response.data, "response");
+        return dispatch({
+          type: FETCH_CURRENCY,
+          data: response.data
+        });
+      })
+
+      .catch(response =>
+        dispatch({
+          type: FETCH_CURRENCY_FAILURE,
+          error: response.error
+        })
+      );
   };
 }
--- src/reducers/currencyReducer.js
+++ src/reducers/currencyReducer.js
@@ -1,11 +1,11 @@
 import { FETCH_CURRENCY } from "../actions/index";
 
 export default function(state = [], action) {
+  console.log(action, "action");
+
   switch (action.type) {
     case FETCH_CURRENCY:
-      console.log(action.payload);
       return action.payload.data;
-
     default:
       return state;
   }
